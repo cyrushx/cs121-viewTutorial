70dfbe952c569c790a8f21836b4e6b54e2590b62
Cyrus Huang <cyrushuang@Cyruss-MacBook-Air.local>
2014-09-10 09:00:37 -0700

    Initial Commit

---
+++ viewTutorial/CHViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHViewController.h
+//  viewTutorial
+//
+//  Created by Cyrus Huang on 9/10/14.
+//  Copyright (c) 2014 Cyrus Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHViewController : UIViewController
+
+@end
---
+++ viewTutorial/CHViewController.m
@@ -0,0 +1,29 @@
+//
+//  CHViewController.m
+//  viewTutorial
+//
+//  Created by Cyrus Huang on 9/10/14.
+//  Copyright (c) 2014 Cyrus Huang. All rights reserved.
+//
+
+#import "CHViewController.h"
+
+@interface CHViewController ()
+
+@end
+
+@implementation CHViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
