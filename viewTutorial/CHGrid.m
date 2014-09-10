ab466c58b849913a5bec77125bb785e6ffd11e0a
Cyrus Huang <cyrushuang@Cyruss-MacBook-Air.local>
2014-09-10 09:21:53 -0700

    finish view tutorial

---
+++ viewTutorial/CHGrid.m
@@ -0,0 +1,31 @@
+//
+//  CHGrid.m
+//  viewTutorial
+//
+//  Created by Cyrus Huang on 9/10/14.
+//  Copyright (c) 2014 Cyrus Huang. All rights reserved.
+//
+
+#import "CHGrid.h"
+
+@implementation CHGrid
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
--- viewTutorial/CHViewController.m
+++ viewTutorial/CHViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "CHViewController.h"
+#import "CHGrid.h"
 
-@interface CHViewController ()
+@interface CHViewController () {
+    UIButton* _button;
+    UIView* _gridView;
+}
 
 @end
 
@@ -20,12 +24,41 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    // create grid view
+    CGRect frame = self.view.frame;
+    CGFloat x    = CGRectGetWidth(frame) * .1;
+    CGFloat y    = CGRectGetHeight(frame) * .1;
+    CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame)) * .8;
+    
+    CGRect gridFrame = CGRectMake(x, y, size, size);
+    
+    // create grid view
+    _gridView = [[CHGrid alloc] initWithFrame:gridFrame];
+    _gridView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:_gridView];
+    
+    // create button
+    CGFloat buttonSize = size/5.0;
+    CGRect buttonFrame = CGRectMake(0, 0, buttonSize, buttonSize);
+    _button = [[UIButton alloc] initWithFrame:buttonFrame];
+    _button.backgroundColor = [UIColor redColor];
+    [_gridView addSubview:_button];
+    
+    // create target for button
+    [_button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed:(id)sender
+{
+    NSLog(@"You pressed the button");
 }
 
 @end
